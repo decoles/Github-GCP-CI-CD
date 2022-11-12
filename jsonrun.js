92a041e672f9add5baa18d4a2f3bb009ff5d06a7
David Coles <davidepcoles@gmail.com>
2022-11-11 22:20:48 -0800

    Update jsonrun.js

--- jsonrun.js
+++ jsonrun.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 80;
-var date = Date.now();
 
 app.get('/', (req, res) => {
     res.json({ 
     message: 'My name is David Coles', 
-    timestamp: date,
+    timestamp: Date.now(),
     });
 });
 
