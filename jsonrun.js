bb79afea025e34ff5bb0fbc7756c8c98fd34c798
decoles <davidepcoles@gmail.com>
2022-11-11 21:59:05 -0800

    added number to js

--- jsonrun.js
+++ jsonrun.js
@@ -5,7 +5,7 @@ const port = 80;
 app.get('/', (req, res) => {
     res.json({ 
     message: 'My name is David Coles', 
-    timestamp: `${Date.now().toPrecision(13)}`,
+    timestamp: `${Number(Date.now())}`,
     });
 });
 
