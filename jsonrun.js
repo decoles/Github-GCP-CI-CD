ef45e76b2b0e4e38a4077c9244430dfc3ba88c9e
David Coles <davidepcoles@gmail.com>
2022-11-13 17:42:04 -0800

    Update jsonrun.js

--- jsonrun.js
+++ jsonrun.js
@@ -4,7 +4,8 @@ const port = 80;
 
 app.get('/', (req, res) => {
     res.json({ 
-
+    message: 'My name is David Coles', 
+    timestamp: Date.now(),
     GCPBuildTest: 'Update without manual push succesful'
     });
 });
